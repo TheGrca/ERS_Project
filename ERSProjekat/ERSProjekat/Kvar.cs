55353da6bfd180ab1327ded34b991bb487dc5a28
Ivan <ivangrcic6@gmail.com>
2023-12-06 20:00:10 +0100

    Prvi commit, napravljena klasa Kvar i pocetni dio programa

---
+++ ERSProjekat/ERSProjekat/Kvar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProjekat
+{
+
+    public enum Status
+    {
+        Nepotvrdjen,
+        U_popravci,
+        Testiranje,
+        Zatvoreno
+    }
+
+    internal class Kvar
+    {
+        private int IDKvara { get; set;  }
+        private DateTime vremeKvara { get; set; }
+        private Status Status { get; set; }
+        private string Kratki_opis { get; set; }
+        private string Elektricni_element { get; set; } // Treba ubaciti klasu
+        private string Opis { get; set; }
+        //private List<Akcija> Akcije { get; set; }
+
+        public Kvar(int id, DateTime datum, Status status, string kratkiOpis, string e_Element, string opis)
+        {
+            IDKvara = id;
+            vremeKvara = datum;
+            Status = status;
+            Kratki_opis = kratkiOpis;
+            Elektricni_element = e_Element;
+            Opis = opis;
+            // = new List<Action>();
+        }
+    }
+
+}
+
---
+++ ERSProjekat/ERSProjekat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProjekat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kratakOpis;
+            string e_Element;
+            string Opis;
+            string izvrsene_Akcije;
+
+            Console.WriteLine("----- OMS - Evidencija kvarova u elektricnoj mrezi -----\n\n\n");
+            Console.WriteLine("Izaberite opciju:\n");
+            Console.WriteLine("1 - Unos podataka o kvaru\n");
+            Console.WriteLine("2 - Lista kvarova\n");
+            string opcija = Console.ReadLine();
+
+            if (opcija == "1")
+            {
+                 Console.WriteLine("----- Unesite kvar -----\n");
+                 Console.WriteLine("Kratak opis kvara: ");
+                 kratakOpis = Console.ReadLine();
+                 Console.WriteLine("Na kojem elektricnom elementu se desio kvar: ");
+                 e_Element = Console.ReadLine();
+                 Console.WriteLine("Opis kvara: ");
+                 Opis = Console.ReadLine();
+                 Console.WriteLine("Izvrsene akcije: ");
+                 izvrsene_Akcije = Console.ReadLine();
+            }
+            else if(opcija == "2")
+            {
+
+            }
+            else
+            {
+                
+            }
+            
+        }
+    }
+}
