9390165176453aa4f6fc27fa0bc36fa7229795f8
olu1c <148206734+olu1c@users.noreply.github.com>
2023-12-06 21:12:14 +0100

    Add files via upload
    Klasa evidencija i osnovni elementi

---
+++ ERSProjekat/EvidencijaElemenata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERSProjekat
+{
+    public enum Napon { srednji_napon,visoki_napon,nizak_napon}
+     public class EvidencijaElemenata
+    {
+        private int id_elementa;
+        private string naziv_elementa;
+        private string tip_elementa;
+        private int geografska_lokacija;
+        private Napon naponski_nivo;
+
+
+        public EvidencijaElemenata()
+        {
+            naponski_nivo = Napon.srednji_napon;
+        }
+
+        public EvidencijaElemenata(int id,string naziv,string tip,int geografska,Napon napon)
+        {
+            id_elementa = id;
+            naziv_elementa = naziv;
+            tip_elementa = tip;
+            geografska_lokacija = geografska;
+            naponski_nivo = napon;
+        }
+    }
+}
